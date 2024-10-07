d77692f51c3c940ca14b8e0b99d502019c30c073
Oscar <159458439+05c4rR@users.noreply.github.com>
2024-10-07 21:23:59 +0200

    Added Jwt created listener Add firstname and lastname to payload

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use App\Entity\User;
+
+class JWTCreatedListener {
+
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {   
+        /** @var User $user */
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+    
+        $payload = $event->getData();
+        $payload['fistname'] = $user->getFirstname();
+        $payload['lastname'] = $user->getLastname();
+    
+        $event->setData($payload);
+    
+        $header = $event->getHeader();
+    
+        $event->setHeader($header);
+    }
+
+}
